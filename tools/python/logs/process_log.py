c14ae06d591523bd57a7b2814a16903171e2bd7e
NMoroney <n8mm@comcast.net>
2017-04-30 15:31:11 -0700

    Add files via upload

---
+++ tools/python/logs/process_log.py
@@ -0,0 +1,57 @@
+# python process_log.py > cleaned.csv
+#
+import re  # regular expressions
+
+# print "process log : [1704]"
+
+log_name = "primaryPP04272017_2.log"
+# print "log name : " + log_name
+
+with open (log_name, "r") as myfile:
+  data = myfile.read()
+# print "log file read in as data"
+
+# start of table tag is: _predict#
+# end of table tage is: -----------
+# to specify everything between: .*?
+# the flags of re.DOTALL is required to search across newlines in data
+#
+tables = re.findall('_predict#.*?----------', data, flags=re.DOTALL)
+number_tables = len(tables)
+# print "found " + str(number_tables) + " tables in the log"
+
+# print "first table "
+# print tables[0]
+
+labels = []
+for table in tables :
+  label = re.findall('_predict#(.*) \\|', table)
+  labels.append(label)
+# print "first and last table labels : "
+# print labels[0]
+# print labels[number_tables-1]
+
+j = 0
+cleaned = []
+for table in tables :
+  rows = table.splitlines()
+  number_rows = len(rows) - 1
+  i = 0
+  label = ''.join(labels[j])
+  for row in rows :
+    if (i > 0) and (i < number_rows) :
+      out = ""
+      halves = row.split('|')
+      if '#' in halves[0] :
+        text = halves[0].split('#')
+      else :
+        text = halves[0].split()
+      out += label + ", " + text[0].strip() + ", " 
+      out += text[1].replace(",",";").strip() + ", "
+      values = halves[1].split()
+      for value in values :
+        out += value + ", "
+      print out[:-2]
+    i += 1
+  j += 1
+
